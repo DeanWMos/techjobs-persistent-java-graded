26225d252ebd1ff9827dd8fda843c9e1f6bdff12
deanw <deanwademosley@gmail.com>
2022-11-14 15:45:05 -0600

    all tests passed

--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -1,17 +1,16 @@
 package org.launchcode.techjobs.persistent.models;
 
-import org.hibernate.validator.constraints.Length;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Objects;
 
-
 @MappedSuperclass
 
 public abstract class AbstractEntity {
-    @ID
+    @Id
     @GeneratedValue
     private int id;
 
