9bab63c1183df3acca11bfb0fa13acef79c0d2c1
deanw <deanwademosley@gmail.com>
2022-11-14 01:29:09 -0600

    working on part 2, added entity

--- src/main/java/org/launchcode/techjobs/persistent/models/data/SkillRepository.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/data/SkillRepository.java
@@ -1,8 +1,11 @@
 package org.launchcode.techjobs.persistent.models.data;
 
+import org.launchcode.techjobs.persistent.models.Entity;
 import org.launchcode.techjobs.persistent.models.Skill;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+@Entity
 
 public interface SkillRepository extends CrudRepository<Skill, Integer> {
 }
