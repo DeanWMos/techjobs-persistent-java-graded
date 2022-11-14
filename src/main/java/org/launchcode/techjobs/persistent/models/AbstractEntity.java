116fbfa748125ff4292a337dc52ecfa649232fa4
deanw <deanwademosley@gmail.com>
2022-11-14 00:36:00 -0600

    added annonations

--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -1,12 +1,19 @@
 package org.launchcode.techjobs.persistent.models;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
 import java.util.Objects;
 
 
+@MappedSuperclass
 public abstract class AbstractEntity {
-
+@ID
+@GeneratedValue
     private int id;
 
+    @NotBlank
+    @Length(min = 1, max = 255)
     private String name;
 
     public int getId() {
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,18 @@
 package org.launchcode.techjobs.persistent.models;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
 public class Employer extends AbstractEntity {
+    @NotBlank
+    @Length(min = 1, max = 125)
+    public String location;
+
+    public Employer() {}
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 
 }
